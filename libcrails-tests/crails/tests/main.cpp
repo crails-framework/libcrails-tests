ba0477f28faddc8d10d94eca37e20f45c7f77a81
plaristote <michael@unetresgrossebite.com>
2022-12-20 17:18:43 +0100

    update uses of Server using Singleton

--- libcrails-tests/crails/tests/main.cpp
+++ libcrails-tests/crails/tests/main.cpp
@@ -1,5 +1,4 @@
 #include "runner.hpp"
-#include <crails/server.hpp>
 #include <crails/logger.hpp>
 #include <crails/environment.hpp>
 
@@ -7,15 +6,11 @@ using namespace std;
 using namespace Crails;
 using namespace Crails::Tests;
 
-Server* test_server = nullptr;
-
 int main(int, char**)
 {
-  Server server;
   Runner runner;
   int    exit_status = 1;
 
-  test_server = &server;
   Crails::environment = Crails::Test;
   runner.setup();
   if (runner.execute())
--- libcrails-tests/crails/tests/request.cpp
+++ libcrails-tests/crails/tests/request.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 using namespace Crails;
 
-extern Server* test_server;
-
 Tests::Request::Request(boost::beast::http::verb method, const std::string& uri)
 {
   request.method(method);
@@ -14,14 +12,16 @@ Tests::Request::Request(boost::beast::http::verb method, const std::string& uri)
 
 void Tests::Request::run()
 {
+  auto& test_server = Server::singleton::require();
   const Router* router = Router::singleton::get();
 
   if (router)
   {
-    auto stub_connection = make_shared<Crails::Connection>(*test_server, request);
-    auto stub_request    = make_shared<Crails::Context>(*test_server, *stub_connection);
+    auto stub_connection = make_shared<Crails::Connection>(test_server, request);
+    auto stub_request    = make_shared<Crails::Context>(test_server, *stub_connection);
 
     stub_request->run();
+    response = stub_request->response.get_raw_response();
     if (!stub_request->handled)
       throw RouteNotFound(std::string(boost::beast::http::to_string(request.method())) + '#' + std::string(request.target()));
   }
