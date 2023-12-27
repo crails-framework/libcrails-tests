40f4bfbb75aff7fa2b04608b1750616decfa1c43
plaristote <michael@unetresgrossebite.com>
2023-12-27 17:17:17 +0100

    adds support for injecting and inspecting params and session in request tests

--- libcrails-tests/crails/tests/request.cpp
+++ libcrails-tests/crails/tests/request.cpp
@@ -20,7 +20,11 @@ void Tests::Request::run()
     auto stub_connection = make_shared<Crails::Connection>(test_server, request);
     auto stub_request    = make_shared<Crails::Context>(test_server, *stub_connection);
 
+    stub_request->params.as_data().merge(params);
+    stub_request->params.get_session().merge(session);
     stub_request->run();
+    session.as_data().destroy();
+    session.as_data().merge(stub_request->params.get_session());
     response = stub_request->response.get_raw_response();
     if (!stub_request->handled)
       throw RouteNotFound(std::string(boost::beast::http::to_string(request.method())) + '#' + std::string(request.target()));
--- libcrails-tests/crails/tests/request.hpp
+++ libcrails-tests/crails/tests/request.hpp
@@ -30,6 +30,8 @@ namespace Crails
 
       void run();
 
+      DataTree     params;
+      DataTree     session;
       HttpRequest  request;
       HttpResponse response;
     };
